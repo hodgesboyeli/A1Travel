ab2f86d9af78a37767b881836a92e4f0c2636f59
hodgesboyeli <elijah1.hodges@famu.edu>
2023-10-27 00:09:40 -0400

    Initial commit

---
+++ src/main/java/famu/edu/a1travel/A1TravelApplication.java
@@ -0,0 +1,13 @@
+package famu.edu.a1travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1TravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A1TravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/famu/edu/a1travel/A1TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package famu.edu.a1travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1TravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
