d0a3194c3052a3f97db26c8ae8c0aba4e99215a1
hodgesboyeli <elijah1.hodges@famu.edu>
2023-10-31 15:12:19 -0400

    React initial commit

---
+++ a1-travel/src/index.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'mdb-ui-kit/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
