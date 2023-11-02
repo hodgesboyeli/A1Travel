e547d690ca379f75d1be531a29f149d5c72f9642
Donovan Hood <70450120+dhood12@users.noreply.github.com>
2023-11-02 16:03:39 -0400

    Update App.js
    Created the NavBar

--- a1-travel/src/App.js
+++ a1-travel/src/App.js
@@ -6,6 +6,21 @@ import Main from "./pages/Main";
 function App() {
     return (
         <Router>
+            <div className="header">
+                <nav className="navbar" style={{backgroundColor: "#1E71EE"}}>
+                    <div className="container-fluid">
+                        <a className="navbar-brand" >Home </a>
+                        <a className="navbar-brand" >Past Bookings </a>
+                        <a className="navbar-brand" >Support </a>
+                        <a className="navbar-brand" >Support </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                </nav>
+            </div>
             <Routes>
                 <Route element={<Home />} path='/home'/>
                 <Route element={<Main />} path=''/>
