3f9eba2e2e7c8f1e8567fe364a08ab4bac0bcd5a
Donovan Hood <70450120+dhood12@users.noreply.github.com>
2023-11-02 16:38:33 -0400

    Made some organizational edits.

--- a1-travel/src/App.js
+++ a1-travel/src/App.js
@@ -2,40 +2,17 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Main from "./pages/Main";
+import Menu from "./pages/Menu";
+import Login from "./pages/Login";
 
 function App() {
     return (
         <Router>
-            <div className="header">
-                <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#1E71EE"}}>
-                    <a className="navbar-brand" href="#">A1 Travel</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Past Bookings</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Support</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Profile</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Settings</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+            <Menu />
             <Routes>
                 <Route element={<Home />} path='/home'/>
                 <Route element={<Main />} path=''/>
+                <Route element={<Login />} path='/login'/>
             </Routes>
         </Router>
     );
---
+++ a1-travel/src/pages/Menu.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function Menu(){
+    return (
+        <div className="header">
+            <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#1E71EE"}}>
+                <a className="navbar-brand mr-auto" href="#">A1 Travel</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link px-3 py-2" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link px-3 py-2" href="#">Past Bookings</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link px-3 py-2" href="#">Support</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-3 py-2" href="#">Profile</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link px-3 py-2" href="#">Settings</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
