a326d50104b6952620c1b45786a75582855b8612
YonYon2 <yonathantorr@gmail.com>
2023-11-02 15:16:31 -0400

    Install dependencies inside frontend folder
    Also added some react pages to test. Currently launches but does not display.

--- a1-travel/src/App.js
+++ a1-travel/src/App.js
@@ -1,6 +1,17 @@
+import React from 'react';
+import {Route, Router, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <></>
+    <Router>
+      <div>
+        Something
+      <Routes>
+        <Route element={<Home />} path='/home' />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
