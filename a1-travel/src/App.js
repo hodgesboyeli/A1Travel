17a6bb73f227613b6009cb4f1c7e5ad278f352a0
Donovan Hood <70450120+dhood12@users.noreply.github.com>
2023-11-02 15:44:13 -0400

    Created the "landing" page for the application.

--- a1-travel/src/App.js
+++ a1-travel/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
+import Main from "./pages/Main";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route element={<Home />} path='/home'/>
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route element={<Home />} path='/home'/>
+                <Route element={<Main />} path=''/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- a1-travel/src/pages/Main.js
+++ a1-travel/src/pages/Main.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home(){
+function Main(){
     return (
         <div>
-            Hello!
+            Updated text...
         </div>
     );
 }
 
-export default Home;
+export default Main;
