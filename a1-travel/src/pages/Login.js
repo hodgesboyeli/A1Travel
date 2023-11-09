cf431ee0cbe7ab6b4c60618fe8f7cd1fd6052364
hodgesboyeli <elijah1.hodges@famu.edu>
2023-11-09 16:45:27 -0500

    added modal when signing in

--- a1-travel/src/pages/Login.js
+++ a1-travel/src/pages/Login.js
@@ -1,97 +1,140 @@
-import React, {useRef, useState} from 'react';
-import {useAuth} from './AuthContext'
-import { Link, useNavigate } from 'react-router-dom'
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-import axios from "axios"
+import React, { useRef, useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import axios from 'axios';
+import { Modal } from 'bootstrap';
 
 export default function Login() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const {login} = useAuth()
-    const [error, setError] = useState('')
-    const [loading,setLoading] = useState(false)
-    const navigate = useNavigate()
-    const currentUser = useAuth()
-    const [user,setUser] = useState({})
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
     const auth = getAuth();
-    async function handleSubmit(e) { // added async to fix await error
-        e.preventDefault()
+    const modalRef = useRef(null);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
         try {
-            setError('')
-            setLoading(true)
-            console.log(emailRef.current.value)
-            console.log(passwordRef.current.value)
-            await signInWithEmailAndPassword(getAuth(), emailRef.current.value, passwordRef.current.value);
+            setError('');
+            setLoading(true);
+
+            await signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value);
+
             try {
-                var response = await axios.get('http://localhost:8080/api/user/email/'+emailRef.current.value)
-                if(response.status === 404) throw response.statusText
+                const response = await axios.get(`http://localhost:8080/api/user/email/${emailRef.current.value}`);
+                if (response.status === 404) throw response.statusText;
 
-                let user = response.data
+                const fetchedUser = response.data;
 
-                console.log(user.email)
-                if (user.email !== emailRef.current.value ) throw "Invalid Credentials";
+                if (fetchedUser.email !== emailRef.current.value) throw 'Invalid Credentials';
 
-                if(!user.isActive) throw 'User is Deactivated or Paused';
+                if (!fetchedUser.isActive) throw 'User is Deactivated or Paused';
 
-                sessionStorage.clear()
-                sessionStorage.setItem("role",user.role)
+                sessionStorage.clear();
+                sessionStorage.setItem('role', fetchedUser.role);
 
-            } catch(err) {
-                console.log(err)
-                setError(err)
+                if (sessionStorage.getItem('role') === "Customer") {
+                    navigate('/home');
+                } else if (sessionStorage.getItem('role') === "Administrator") {
+                    navigate('/admin-home');
+                } else if (sessionStorage.getItem('role') === "Travel Administrator") {
+                    navigate('/ta-home/');
+                } else {
+                    console.log("Invalid user role value, returning to login.")
+                    navigate('/login');
+                }
+            } catch (err) {
+                console.log(err);
+                setError('Invalid email or password');
+                showModal();
             }
 
-            if (sessionStorage.getItem('role') ==="Customer"){
-                navigate('/home')
-            }else if (sessionStorage.getItem('role') === "Administrator"){
-                navigate('/admin-home')
-            }else if (sessionStorage.getItem('role') === "Travel Administrator"){
-                navigate('/ta-home/')
-            }else{
-                console.log("Invalid user role value, returning to login.")
-                navigate('/login')
-            }
         } catch {
-            setError('Failed to sign in')
+            setError('Invalid Email or Password. Please Try again.');
+            showModal();
+        } finally {
+            setLoading(false);
         }
-        setLoading(false)
+    }
+
+    const showModal = () => {
+        if (modalRef.current) {
+            const modal = new Modal(modalRef.current);
+            modal.show();
+        }
     };
-    return (<div className="page-style">
-        <div className="container justify-content-center">
-            <div className="container-fluid d-flex justify-content-center">
-                <p className="brand-name h2 mb-3" style={{color: "#FF6C37", paddingTop: 55}}>
-                    A1 TRAVEL
-                </p>
+
+    const handleCloseModal = () => {
+        if (modalRef.current) {
+            const modal = Modal.getInstance(modalRef.current);
+            if (modal) {
+                modal.hide();
+            }
+        }
+    };
+
+    useEffect(() => {
+        return () => {
+            handleCloseModal();
+        };
+    }, []);
+
+    return (
+        <div className="page-style">
+            <div className="container justify-content-center">
+                <div className="container-fluid d-flex justify-content-center">
+                    <p className="brand-name h2 mb-3" style={{ color: '#FF6C37', paddingTop: 55 }}>
+                        A1 TRAVEL
+                    </p>
+                </div>
+                <div className="text-center mt-3">
+                    <img src={`${process.env.PUBLIC_URL}/A1Logo.png`} className="img-fluid" alt="A1 Travel Logo" />
+                </div>
+                <div className="container-fluid d-flex justify-content-center">
+                    <p className="h2" style={{ color: 'white', paddingTop: 55 }}>
+                        LOGIN
+                    </p>
+                </div>
             </div>
-            <div className="text-center mt-3">
-                <img src={process.env.PUBLIC_URL + "/A1Logo.png"} className="img-fluid" alt="A1 Travel Logo" />
-            </div>
-            <div className="container-fluid d-flex justify-content-center">
-                <p className="h2" style={{color: "white", paddingTop: 55}}>
-                    LOGIN
-                </p>
+
+            <div className="signin-form-movement container-fluid d-flex justify-content-center">
+                <form className="w-25 row" onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <input type="email" className="form-control" id="email" placeholder="USERNAME" ref={emailRef} />
+                    </div>
+                    <div className="mb-3">
+                        <input type="password" className="form-control" id="password" placeholder="PASSWORD" ref={passwordRef} />
+                    </div>
+                    <div className="text-center" style={{ marginTop: 40 }}>
+                        <button disabled={loading} type="submit" className="btn btn-sm custom-button">
+                            SIGN IN
+                        </button>
+                    </div>
+                    <div className="text-center mt-4">
+                        <Link to="/signup">CREATE AN ACCOUNT</Link>
+                    </div>
+                </form>
+
+                <div className="modal fade" ref={modalRef} tabIndex="-1" aria-labelledby="errorModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="errorModalLabel">
+                                    Error
+                                </h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">{error && <p>{error}</p>}</div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="signin-form-movement container-fluid d-flex justify-content-center" >
-            <form className="w-25 row" onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <input type="email" className="form-control" id="email" placeholder="USERNAME"
-                    ref={emailRef}/>
-                </div>
-                <div className="mb-3">
-                    <input type="password" className="form-control" id="password" placeholder="PASSWORD"
-                    ref={passwordRef}/>
-                </div>
-                <div className="text-center" style={{marginTop: 40}}>
-                    <button disabled={loading} type='submit' className="btn btn-sm custom-button">
-                        SIGN IN
-                    </button>
-                </div>
-                <div className="text-center mt-4">
-                    <Link to="/signup">CREATE AN ACCOUNT</Link>
-                </div>
-            </form>
-        </div>
-    </div>);
+    );
 }
