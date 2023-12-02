2e05d7eb553535a3a8d4d90b8113eac7aab0f45a
hodgesboyeli <elijah1.hodges@famu.edu>
2023-12-02 14:34:11 -0500

    Update TripsService.java

--- src/main/java/famu/edu/a1travel/Service/TripsService.java
+++ src/main/java/famu/edu/a1travel/Service/TripsService.java
@@ -1,5 +1,25 @@
 package famu.edu.a1travel.Service;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import famu.edu.a1travel.Model.Trips;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+
 //get all, get all by id, etc.
+@Service
 public class TripsService {
+    private Firestore db = FirestoreClient.getFirestore();
+    public String createTrip(Trips trip) throws ExecutionException, InterruptedException {
+        String tripId = null;
+
+        ApiFuture<DocumentReference> future = db.collection("Trips").add(trip);
+        DocumentReference tripRef = future.get();
+        tripId = tripRef.getId();
+
+        return tripId;
+    }
 }
