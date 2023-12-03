5fac5771a4fe27c22cab1f5fce195c821b4fefbd
hodgesboyeli <elijah1.hodges@famu.edu>
2023-12-03 18:59:05 -0500

    Update AnnouncementsService.java

--- src/main/java/famu/edu/a1travel/Service/AnnouncementsService.java
+++ src/main/java/famu/edu/a1travel/Service/AnnouncementsService.java
@@ -1,8 +1,11 @@
 package famu.edu.a1travel.Service;
 
 import com.google.api.core.ApiFuture;
+import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
 import famu.edu.a1travel.Model.RestAnnouncements;
+import famu.edu.a1travel.Model.Users;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,16 +19,17 @@ public class AnnouncementsService {
         this.db = db;
     }
     public ArrayList<RestAnnouncements> getAnnouncements() throws ExecutionException, InterruptedException {
-
         Query query = db.collection("Announcements");
         ApiFuture<QuerySnapshot> future = query.get();
         List<QueryDocumentSnapshot> documents = future.get().getDocuments();
 
-        ArrayList<RestAnnouncements> destinations = (documents.size() > 0) ? new ArrayList<>() : null;
+        ArrayList<RestAnnouncements> announcements = new ArrayList<>();
 
-        for (QueryDocumentSnapshot doc : documents)
-            destinations.add(doc.toObject(RestAnnouncements.class));
+        for (QueryDocumentSnapshot doc : documents) {
+            announcements.add(getAnnouncements(doc));
+        }
 
-        return destinations;
+        return announcements;
     }
+
 }
