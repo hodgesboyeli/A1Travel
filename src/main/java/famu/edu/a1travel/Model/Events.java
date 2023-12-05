464cc006214108053ff0b90022a3e731e5a342d5
Donovan Hood <70450120+dhood12@users.noreply.github.com>
2023-12-05 15:48:44 -0500

    Timestamp altercations fixed.

--- src/main/java/famu/edu/a1travel/Model/Events.java
+++ src/main/java/famu/edu/a1travel/Model/Events.java
@@ -18,20 +18,9 @@ public class Events {
     private @Nullable String eventId;
     private int capacity;
     private String description;
-    @JsonFormat(pattern = "MMMM dd, yyyy 'at' hh:mm:ss a 'UTC-5'", timezone = "UTC-5")
     private Timestamp eventEnd;
-    public void setEventEnd(String eventEnd) throws ParseException {
-        this.eventEnd = Timestamp.fromProto(Timestamps.parse(eventEnd));
-    }
-
     private String eventName;
-
-    @JsonFormat(pattern = "MMMM dd, yyyy 'at' hh:mm:ss a 'UTC-5'", timezone = "UTC-5")
     private Timestamp eventStart;
-    public void setEventStart(String eventStart) throws ParseException {
-        this.eventStart = Timestamp.fromProto(Timestamps.parse(eventStart));
-    }
-
     private String eventType;
     private String image;
     private String address;
