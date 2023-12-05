4afac825b37d14786ce730c3a46792457ca3b5b6
hodgesboyeli <elijah1.hodges@famu.edu>
2023-12-05 11:26:38 -0500

    budget page session storage complete

--- a1-travel/src/pages/Customer/CustBudget.js
+++ a1-travel/src/pages/Customer/CustBudget.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from "../Navbar";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function CustBudget() {
     const [budget, setBudget] = useState('');
@@ -13,16 +13,39 @@ export default function CustBudget() {
         // Format the number with commas
         return numericValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     };
+
     const handleBudgetChange = (e) => {
-        const val = e.target.value.replace(/,/g,'');
+        const val = e.target.value.replace(/,/g, '');
         setBudget(formatNumber(val));
     };
 
     const handleSubmit = () => {
-        // Handle the submit action here
-        // add logic for storing budget number into session storage
-        console.log('Budget submitted:', budget);
+        // Parse the budget string to a number
+        const budgetNumber = parseInt(budget.replace(/,/g, ''), 10);
 
+        // Check if the entered value is a valid number
+        if (!isNaN(budgetNumber)) {
+            console.log('Valid budget input:', budgetNumber);
+
+            // Store the budget number in session storage using the field name from the database
+            sessionStorage.setItem('budget', budgetNumber);
+            console.log('Budget stored in session storage:', budgetNumber);
+
+            // Navigate to the destination page
+            navigate('/destination');
+        } else {
+            // Handle invalid budget input (optional)
+            console.log('Invalid budget input');
+            // You can show an error message to the user if needed
+        }
+    };
+
+    const handleNoBudgetContinue = () => {
+        // Set the budget in session storage to -1
+        sessionStorage.setItem('budget', -1);
+        console.log('Budget stored in session storage:', -1);
+
+        // Navigate to the destination page
         navigate('/destination');
     };
 
@@ -30,7 +53,7 @@ export default function CustBudget() {
         <>
             <Navbar/>
             <div className="mt-5 text-center">
-                    <h1 className="text-center">Budget</h1>
+                <h1 className="text-center">Budget</h1>
                 <section className="my-5">
                     <div className="d-flex flex-column align-items-center">
                         <h2 className="mb-3">WHAT IS YOUR BUDGET?</h2>
@@ -43,18 +66,16 @@ export default function CustBudget() {
                                 placeholder='Enter amount'
                                 onChange={handleBudgetChange}
                             />
-                                <button className="btn btn-success" onClick={handleSubmit}>
-                                    Confirm
-                                </button>
+                            <button className="btn btn-success" onClick={handleSubmit}>
+                                Confirm
+                            </button>
                         </div>
                     </div>
                 </section>
                 <div className="container-fluid d-flex justify-content-center">
-                    <Link to="/destination">
-                        <button type="submit" className="btn btn-md custom-button">
-                            Next
-                        </button>
-                    </Link>
+                    <button className="btn btn-link" type="button" onClick={handleNoBudgetContinue}>
+                        No budget? CONTINUE HERE
+                    </button>
                 </div>
             </div>
         </>
--- a1-travel/src/pages/Customer/CustDestination.js
+++ a1-travel/src/pages/Customer/CustDestination.js
@@ -11,6 +11,9 @@ export default function CustDestination() {
                 <div className="container-fluid d-flex justify-content-center mt-5 mb-3">
                     <h1>Where do you want to go?</h1>
                 </div>
+                <div className="container-fluid d-flex justify-content-center">
+                    {/*Search bar logic here*/}
+                </div>
                 <div className="text-center" style={{marginTop: 40}}>
                     <Link to="/flight">
                         <button type="submit" className="btn btn-md custom-button">
