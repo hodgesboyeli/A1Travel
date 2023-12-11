87d58e906a6f10b2913cc969b8ea2259aff18f1d
hodgesboyeli <elijah1.hodges@famu.edu>
2023-12-10 23:06:04 -0500

    Update CustLodging.js

--- a1-travel/src/pages/Customer/CustLodging.js
+++ a1-travel/src/pages/Customer/CustLodging.js
@@ -10,24 +10,30 @@ export default function CustLodging(){
                 <div className="container-fluid d-flex justify-content-center mt-5 mb-3">
                     <h1>Where would you like to stay</h1>
                 </div>
-                <div className="text-center" style={{ marginTop: 40 }}>
-                    <Link to="/hotel">
-                        <button type="submit" className="btn btn-md custom-button">
-                            Hotel
-                        </button>
-                    </Link>
-                    <Link to="/airbnb">
-                        <button type="submit" className="btn btn-md custom-button">
-                            Airbnb
-                        </button>
-                    </Link>
-                    <Link to="/event">
-                        <div className="container-fluid d-flex justify-content-center">
-                            <button className="btn btn-link" type="button">
-                                Have a place to stay? CONTINUE HERE
+                <div className="mt-5">
+                    <div className="text-center">
+                        <Link to="/hotel">
+                            <button type="submit" className="btn btn-md custom-button" style={{ fontSize: 40, paddingLeft: 300, paddingRight: 300, backgroundColor: "#1E71EE" }}>
+                                Hotel
                             </button>
-                        </div>
-                    </Link>
+                        </Link>
+                    </div>
+                    <div className="text-center" style={{ marginTop: 40 }}>
+                        <Link to="/airbnb">
+                            <button type="submit" className="btn btn-md custom-button" style={{ fontSize: 40, paddingLeft: 300, paddingRight: 300 }}>
+                                Airbnb
+                            </button>
+                        </Link>
+                    </div>
+                    <div className="text-center" style={{ marginTop: 40 }}>
+                        <Link to="/event">
+                            <div className="container-fluid d-flex justify-content-center">
+                                <button className="btn btn-link" type="button">
+                                    Have a place to stay? CONTINUE HERE
+                                </button>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
