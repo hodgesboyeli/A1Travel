8873dcb7a481d84c247b1ee93a88c6cacf9e0159
YonYon2 <yonathantorr@gmail.com>
2023-12-11 22:46:58 -0500

    Add Checkout Page UI
    Adding bootstrap stuff to make page functional

--- a1-travel/src/index.js
+++ a1-travel/src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/js/bootstrap.min'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'mdb-ui-kit/css/mdb.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import React from 'react';
--- a1-travel/src/pages/Customer/CustCheckout.js
+++ a1-travel/src/pages/Customer/CustCheckout.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React/*, {useState}*/, {useEffect, useState} from 'react';
 import Navbar from "../../Navbars/Navbar";
 import {Link} from "react-router-dom";
+import {array} from "mdb-ui-kit/src/js/mdb/util";
 
 export default function CustCheckout(){
     const [car, setCar] = useState(null);
@@ -8,19 +9,74 @@ export default function CustCheckout(){
     const [flights, setFlights] = useState([]);
     const [events,setEvents] = useState([]);
     const [trains, setTrains] = useState([]);
+    const [view,setView] = useState({
+        flight:false
+    });
 
-    /*useEffect(()=>{
+    useEffect(()=>{
 
-    },[]);*/
+    },[]);
 
+    /*const toggleFlight = ()=>{
+        setView();
+    };*/
+
+    const right = "bi bi-caret-right-fill";
+    //const down = "bi bi-caret-down-fill";
     return (
         <>
             <Navbar />
-            <div className="" >
-                <div className="container-fluid d-flex justify-content-center mt-5 mb-3">
-                    <h1>Checkout</h1>
+            <div className="container w-75">
+                <h1 className="d-flex mt-5 mb-5 justify-content-center">
+                    Checkout
+                </h1>
+                {/*FLIGHTS */}
+                <h3 id="flights" className="row align-items-end">
+                    <div className="row">
+
+                    <span className="col">
+                        <a href='#flights' className='dropdown-arrow p-2'>
+                            <i className={`${right}`}/>
+                        </a>
+                        Flights
+                    </span>
+                        <span className="col text-end">
+
+                    </span>
+                    </div>
+                    <div className="collapsed row" id="flightCollapse">
+                        Something
+                    </div>
+                </h3>
+                {/*EVENTS */}
+                <h3 id="events" className="row align-items-end">
+                    <div className="row">
+                        <span className="col">
+                            <a href='#' className='dropdown-arrow p-2'>
+                                <i className={`${right}`}/>
+                            </a>
+                            Flights
+                        </span>
+                        <span className="col text-end">
+
+                        </span>
+                    </div>
+                    <div className="row">
+                        Something
+                    </div>
+                </h3>
+                {/*TOTAL row*/}
+                <hr/>
+                <div className="row align-items-end">
+                    <span className="total col ps-5">
+                        Total
+                    </span>
+                    <span className="total col text-end">
+                        $456.00
+                    </span>
                 </div>
-                <div className="text-center" style={{ marginTop: 40 }}>
+                {/*Confirm Button*/}
+                <div className="text-center">
                     <Link to="/home">
                         <button type="submit" className="btn btn-md custom-button">
                             Confirm Trip
