1312f503476dd26825a0536278ea49823253ebb6
hodgesboyeli <elijah1.hodges@famu.edu>
2023-12-15 02:35:43 -0500

    Update CustBookings.js

--- a1-travel/src/pages/Customer/CustBookings.js
+++ a1-travel/src/pages/Customer/CustBookings.js
@@ -4,60 +4,110 @@ import Axios from 'axios';
 
 export default function CustBookings() {
     const [trips, setTrips] = useState([]);
-    const [searchBy, setSearchBy] = useState(''); // Set the default search criteria
+    const [searchBy, setSearchBy] = useState('');
     const searchQuery = useRef();
     const [filteredTrips, setFilteredTrips] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         const fetchTrips = async () => {
             try {
-                const response = await Axios.get('http://localhost:8080/api/trip/');
-                let data = response.data.trips;
-
+                const response = await Axios.get('http://localhost:8080/api/trip/?user=' + sessionStorage.getItem('email'));
+                const data = response.data.trips;
 
                 setTrips(data);
-                sessionStorage.setItem('allTrips',JSON.stringify(data));
+                sessionStorage.setItem('allTrips', JSON.stringify(data));
             } catch (e) {
-                console.log('error',e);
+                console.log('error', e);
             }
         };
+
         const temp = sessionStorage.getItem('allTrips');
-        if (!temp)
-            fetchTrips().then();
-        else {
-            setTrips(prev=> JSON.parse(temp));
+        if (!temp) {
+            fetchTrips();
+        } else {
+            setTrips(JSON.parse(temp));
             setFilteredTrips([]);
-            console.log(trips);
         }
-    },[])
+    }, []);
+
+    useEffect(() => {
+        console.log('FilteredTrips updated:', filteredTrips);
+    }, [filteredTrips]);
 
     const handleSearchByChange = (event) => {
         setSearchBy(event.target.value);
     };
 
-    const handleSearch = async(e) => {
-        e.preventDefault();
-        // Perform a Firestore query from backend matching each user's field with a value
-        const search = searchQuery.current.value;
-        setFilteredTrips(filterTripsByStringMatch(searchBy,search));
-    };
-    // Filter users based on string value closeness
+    // Filter trips based on string value closeness
     const filterTripsByStringMatch = (field, searchString) => {
-        // Sort users by the specified field
+        // Sort trips by the specified field
         const sortedTrips = [...trips].sort((a, b) => {
             if (a[field] < b[field]) return -1;
             if (a[field] > b[field]) return 1;
             return 0;
         });
-        // If searchString is provided, filter the sorted users
+
+        console.log('Sorted trips:', sortedTrips);
+
+        // If searchString is provided, filter the sorted trips
         if (searchString !== '') {
-            return sortedTrips.filter(trip => {
+            const filtered = sortedTrips.filter(trip => {
+                console.log('Filtering by Field:', field, 'Search String:', searchString);
+
+                // Check if the field is defined for the current trip
                 if (!trip[field]) return false;
+
+                // Log the field and search string for debugging
+                console.log('Field:', field, 'Search String:', searchString);
+
+                // Check if the field is an array of references
+                if (Array.isArray(trip[field])) {
+                    const result = trip[field].some(item => {
+                        // Check if the item.id is defined before using it
+                        if (!item.id) return false;
+
+                        // Log the item.id and search string for debugging
+                        console.log('Item ID:', item.id, 'Search String:', searchString);
+
+                        // Perform the search operation
+                        const includes = item.id.toLowerCase().includes(searchString.toLowerCase());
+                        console.log('Includes:', includes);
+                        return includes;
+                    });
+
+                    console.log('Result:', result);
+                    return result;
+                }
+
+                // For non-array fields, perform a regular string match
                 return trip[field].toLowerCase().includes(searchString.toLowerCase());
             });
+
+            console.log('Filtered trips:', filtered);
+            return filtered;
         }
+
         return sortedTrips;
     };
+
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const search = searchQuery.current.value;
+        console.log('Searching for:', search, 'by field:', searchBy);
+
+        // If searchString is provided, filter the trips
+        if (search !== '') {
+            const filtered = filterTripsByStringMatch(searchBy, search);
+            console.log('Filtered trips:', filtered);
+            setFilteredTrips(filtered);
+        } else {
+            // If search is empty, set filteredTrips to the full list of trips
+            console.log('Search is empty. Setting filteredTrips to all trips.');
+            setFilteredTrips(trips);
+        }
+    };
+
 
     return (
         <>
@@ -133,11 +183,11 @@ export default function CustBookings() {
             </div>
             <div className="d-flex flex-column align-content-center">
                 <div className="card-body bg-light">
-                    {trips.length === 0 ? (
+                    {filteredTrips.length === 0 ? (
                         <p>No trips found</p>
                     ) : (
                         <ul className="list-group list-group-flush">
-                            {trips.map((trip, index) => (
+                            {filteredTrips.map((trip, index) => (
                                 <li key={index} className="list-group-item">
                                     <p>Destination: {trip.destination}</p>
                                     <p>Budget: {trip.budget}</p>
